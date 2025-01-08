3d5ceafeea6623002f6528dfd92e077115e6f831
nekowawolf <irukainu20@gmail.com>
2025-01-08 08:25:55 +0700

    feat: add post transaction

--- assets/js/config/url.js
+++ assets/js/config/url.js
@@ -1,1 +1,2 @@
 export let urlGETIncome = "https://nww-sc-8ae1c886f79f.herokuapp.com/savecash/incomes"; 
+export let urlGETTransaction = "https://nww-sc-8ae1c886f79f.herokuapp.com/savecash/transactions";
