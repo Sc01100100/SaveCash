a1286727a4fc1f5ead0b6af31fca7b8c27ded8c7
nekowawolf <irukainu20@gmail.com>
2025-01-08 08:29:44 +0700

    feat: add TableTransaction template for transaction display

---
+++ assets/js/temp/table-transaction.js
@@ -0,0 +1,10 @@
+export let TableTransaction = `
+    <li class="bg-gray-800 p-4 rounded-lg flex justify-between items-center">
+          <div>
+              <span>#CATEGORY#</span>
+              <p class="text-xs mt-2 text-gray-200">#DESC#</p>
+              <p class="text-xs mt-2 text-gray-400">#DATE#</p>
+          </div>
+          <span class="text-red-500">#AMOUNT#</span>
+        </li>
+`;
